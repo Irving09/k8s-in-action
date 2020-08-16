5af6f1ba51af99ebcb8a4419341160ed63f158c5
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2020-08-16 16:56:46 -0700

    initial app commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const os = require('os');
+
+const listenerPort = 8080;
+console.log('Kubia server starting...');
+console.log('Local hostname is', os.hostname());
+console.log(('Listening on port', listenerPort));
+
+const handler = (request, response) => {
+  const clientIp = request.connection.remoteAddress;
+  console.log('Received request for', request.url, 'from', clientIp);
+  response.writeHead(200);
+  response.write('Hey there, this is', os.hostname(), '.');
+  response.write('Your IP is', clientIp, '.');
+  response.end('\n');
+}
+
+const server = http.createServer(handler);
+server.listen(listenerPort);
